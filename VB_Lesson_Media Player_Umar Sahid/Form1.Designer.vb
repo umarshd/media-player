cd52f25cdf5b4ed1a6237bf2e42fd1652843072b
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2021-01-15 23:30:51 +0700

    add all

---
+++ VB_Lesson_Media Player_Umar Sahid/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.buttonAddFile = New System.Windows.Forms.Button()
+        Me.buttonPlay = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(1, 1)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(514, 444)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(516, 1)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(196, 329)
+        Me.ListBox1.TabIndex = 1
+        '
+        'buttonAddFile
+        '
+        Me.buttonAddFile.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.buttonAddFile.Location = New System.Drawing.Point(521, 343)
+        Me.buttonAddFile.Name = "buttonAddFile"
+        Me.buttonAddFile.Size = New System.Drawing.Size(75, 23)
+        Me.buttonAddFile.TabIndex = 2
+        Me.buttonAddFile.Text = "Add File"
+        Me.buttonAddFile.UseVisualStyleBackColor = False
+        '
+        'buttonPlay
+        '
+        Me.buttonPlay.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.buttonPlay.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.buttonPlay.FlatAppearance.BorderSize = 0
+        Me.buttonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.buttonPlay.Location = New System.Drawing.Point(626, 343)
+        Me.buttonPlay.Name = "buttonPlay"
+        Me.buttonPlay.Size = New System.Drawing.Size(75, 23)
+        Me.buttonPlay.TabIndex = 3
+        Me.buttonPlay.Text = "Play"
+        Me.buttonPlay.UseVisualStyleBackColor = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(608, 395)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Umar Sahid"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(640, 421)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "190511081"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(713, 445)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.buttonPlay)
+        Me.Controls.Add(Me.buttonAddFile)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents buttonAddFile As System.Windows.Forms.Button
+    Friend WithEvents buttonPlay As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ VB_Lesson_Media Player_Umar Sahid/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+    Dim total As Integer = 0
+    Dim mp3file(10) As String
+
+    Private Sub AddMedia()
+        Dim curFile As String
+        If (total > 9) Then
+            MessageBox.Show("Jumlah file sudah maksimal")
+            Exit Sub
+        End If
+        OpenFileDialog1.ShowDialog()
+        curFile = OpenFileDialog1.SafeFileName
+
+        ListBox1.Items.Add(curFile)
+        total = total + 1
+        mp3file(total) = OpenFileDialog1.FileName
+
+    End Sub
+
+    Private Sub PlayMedia()
+        Dim i As Integer
+        Dim newPlayList As WMPLib.IWMPPlaylist = AxWindowsMediaPlayer1.playlistCollection.newPlaylist("soundsToPlay")
+
+        For i = 1 To total
+            newPlayList.appendItem(AxWindowsMediaPlayer1.newMedia(mp3file(i)))
+        Next
+
+        'play the list
+        AxWindowsMediaPlayer1.Visible = True
+        AxWindowsMediaPlayer1.currentPlaylist = newPlayList
+        AxWindowsMediaPlayer1.stretchToFit = True
+    End Sub
+
+    Private Sub buttonAddFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonAddFile.Click
+        AddMedia()
+    End Sub
+
+    Private Sub buttonPlay_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonPlay.Click
+        PlayMedia()
+    End Sub
+
+
+End Class
